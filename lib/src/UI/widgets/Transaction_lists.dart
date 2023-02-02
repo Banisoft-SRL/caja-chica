f07a5b145db4139d1ca773143d65b6f71b7ad49b
David Garcia <deivy.code@gmail.com>
2023-02-02 11:09:59 -0400

    Main and Login.dart

---
+++ lib/src/UI/widgets/Transaction_lists.dart
@@ -0,0 +1,23 @@
+import 'package:caja_chica/main.dart';
+import 'package:flutter/material.dart';
+
+void List()=>runApp(MaterialApp(
+  ));
+
+class ListPage extends StatefulWidget {
+  ListPage({Key? key}) : super(key:  key);
+  _ListPageState createState() => _ListPageState();
+}
+class _ListPageState extends State<LoginPage> {
+  bool _loading = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.white,
+      child: Column(children: [
+        Text('hola')
+      ],),),
+    );
+  }
+  }
